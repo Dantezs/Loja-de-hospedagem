7f9af7190a836cea6b2cd02b46b848ea49191b24
Dantezs <126033750+Dantezs@users.noreply.github.com>
2023-04-10 18:52:45 -0300

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,40 @@
+const apikey = 'dbcd8e56';
+const frmPesquisa = document.querySelector("form");
+
+frmPesquisa.onsubmit = (ev) => {
+    ev.preventDefault();
+    
+
+    const pesquisa = ev.target.pesquisa.value;
+
+    if (pesquisa == "") {
+        alert('Preencha o campo!');
+        return;
+    }
+
+    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apikey}`)
+    .then(result => result.json())
+    .then(json => carregaLista(json));
+}
+
+const carregaLista = (json) => {
+    const lista = document.querySelector("div.lista"); 
+    lista.innerHTML = "";
+
+    if (json.Response == 'False') {
+        alert("Nem um filme emcontrado");
+        return
+    }
+
+    json.Search.forEach(element => {
+        console.log(element);
+
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        item.innerHTML = `<img src="${element.Poster}" /><h2>${element.Title}</h2>`;
+
+        lista.appendChild(item)
+        
+    });
+}
